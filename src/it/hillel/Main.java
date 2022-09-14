9e45fedcd553bd2f907d15df4f123df39ec243d0
Your Name <you@example.com>
2022-09-14 14:26:38 +0300

    initial commit -m

---
+++ src/it/hillel/Main.java
@@ -0,0 +1,15 @@
+package it.hillel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	int a = 2;
+	int b = 5;
+	int c = 9;
+	int volume = a*b*c;
+	int lenght = (a + b + c) * 4;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+		System.out.println(lenght);
+    }
+}
