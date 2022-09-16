3ea71be84913318904fe33c17b7c2e8f47ffc627
Your Name <you@example.com>
2022-09-16 13:10:30 +0300

    guess the number-program

--- src/it/hillel/Main.java
+++ src/it/hillel/Main.java
@@ -1,15 +1,23 @@
 package it.hillel;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-	int a = 2;
-	int b = 5;
-	int c = 9;
-	int volume = a*b*c;
-	int lenght = (a + b + c) * 4;
-        System.out.println("Об'єм паралелепіпеда = " + volume);
-		System.out.println(lenght);
-    }
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Write a number from 1 to 6");
+		int number = scanner.nextInt();
+
+		if (number == random.nextInt(6)) {
+			System.out.println("You WIN!");
+		} else {
+			System.out.println("You lost!");
+			System.out.println(random.nextInt(6));
+		}
+	}
 }
